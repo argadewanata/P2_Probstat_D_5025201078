23f63d5220d7086a3e4e658fae2cbda357706b69
Arga Dewanata <70679432+argadewanata@users.noreply.github.com>
2022-05-29 15:51:30 +0700

    Soal 1 Done!

---
+++ D_5025201078.r
@@ -0,0 +1,19 @@
+# =================== Soal 1 ======================
+  # 1A
+    x <- c(78,75,67,77,70,72,78,74,77)
+    y <- c(100,95,70,90,90,90,89,90,100)
+    stdev_soal1 <- sd(x-y)
+    print(stdev_soal1)
+     
+  # 1B
+    var(x)
+    var(y)
+    t.test(x,y,paired = TRUE,var.equal = FALSE)
+     
+  # 1C
+    # (penjelasan di readme.md )
+   
+
+    
+   
+   
